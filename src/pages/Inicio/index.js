771c41f50f28a56c6fc68d07e7768b0de5cbc56f
YuriZem <yurizem333@gmail.com>
2026-03-23 09:42:07 -0300

    pegando dados mockados para visualização

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -3,6 +3,8 @@ import Rodape from "../../components/Rodape";
 import Banner from "../../components/Banner";
 import Card from "../../components/Card";
 import Titulo from "../../components/Titulo";
+import videos from '../../json/db.json' 
+import styles from './inicio.module.css'
 
 function Inicio() {
   return (
@@ -13,7 +15,13 @@ function Inicio() {
         <h1>Um lugar para guardar seus videos e filmes</h1>
       }>
       </Titulo>
-      <Card id='1' titulo='teste 3'></Card>
+      {/* <Card id='1' titulo='teste 3'></Card> */}
+
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return  <Card{...video} key={video.id}></Card>
+        })}
+      </section>
       <Rodape></Rodape>
     </>
     // <></>
