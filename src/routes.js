c776945599938094580ecf71c137c75455416dae
YuriZem <yurizem333@gmail.com>
2026-03-23 14:58:39 -0300

    Criando contexto de favoritos e adicionando na rota

--- src/routes.js
+++ src/routes.js
@@ -5,16 +5,18 @@ import Favoritos from "./pages/Favoritos"
 import Cabecalho from './components/Cabecalho'
 import Rodape from './components/Rodape'
 import Container from "./components/Container"
+import FavoritosProvider from "./contextos/Favoritos"
 function AppRouter() {
   return (
     <BrowserRouter>
       <Cabecalho />
-        <Container>
-
-        <Routes>
-          <Route path="/" element={<Inicio />}></Route>
-          <Route path="/favoritos" element={<Favoritos></Favoritos>}></Route>
-        </Routes>
+      <Container>
+        <FavoritosProvider>
+          <Routes>
+            <Route path="/" element={<Inicio />}></Route>
+            <Route path="/favoritos" element={<Favoritos></Favoritos>}></Route>
+          </Routes>
+        </FavoritosProvider>
       </Container>
       <Rodape />
     </BrowserRouter>
