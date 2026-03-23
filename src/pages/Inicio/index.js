e515d65999b4d6df644307ad460c4fe9df3cdfe6
YuriZem <yurizem333@gmail.com>
2026-03-23 09:18:32 -0300

    adicionando card para listagem de filmes

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,6 +1,7 @@
 import Cabecalho from "../../components/Cabecalho";
 import Rodape from "../../components/Rodape";
 import Banner from "../../components/Banner";
+import Card from "../../components/Card";
 
 function Inicio() {
   return (
@@ -10,6 +11,7 @@ function Inicio() {
       <Inicio>
         <h1>Um lugar para guardar seus videos e filmes</h1>
       </Inicio>
+      <Card id='1' titulo='teste 3'></Card>
       <Rodape></Rodape>
     </>
     // <></>
