fce0aab22621dcf7295ff93f095e6349151bcda0
YuriZem <yurizem333@gmail.com>
2026-03-23 08:57:28 -0300

    adicionando Banner

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,10 +1,12 @@
-import Cabecalho from "../../components/Cabecalho"
-import Rodape from "../../components/Rodape"
+import Cabecalho from "../../components/Cabecalho";
+import Rodape from "../../components/Rodape";
+import Banner from "../../components/Banner";
 
 function Inicio() {
   return (
     <>
       <Cabecalho></Cabecalho>
+      <Banner imagem="home" ></Banner>
       <Rodape></Rodape>
     </>
     // <></>
