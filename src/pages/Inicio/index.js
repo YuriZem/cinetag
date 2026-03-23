1fcd9a801b7bf5cce20d02ca34eadaebb77a9e84
YuriZem <yurizem333@gmail.com>
2026-03-23 09:28:45 -0300

    Correção de bug de componente errado e falta de {} em um children

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -2,15 +2,17 @@ import Cabecalho from "../../components/Cabecalho";
 import Rodape from "../../components/Rodape";
 import Banner from "../../components/Banner";
 import Card from "../../components/Card";
+import Titulo from "../../components/Titulo";
 
 function Inicio() {
   return (
     <>
       <Cabecalho></Cabecalho>
       <Banner imagem="home" ></Banner>
-      <Inicio>
+      <Titulo children={
         <h1>Um lugar para guardar seus videos e filmes</h1>
-      </Inicio>
+      }>
+      </Titulo>
       <Card id='1' titulo='teste 3'></Card>
       <Rodape></Rodape>
     </>
