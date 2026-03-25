4522180fdf858cc4adef67e4e19a86f7efe48956
YuriZem <yurizem333@gmail.com>
2026-03-25 08:46:33 -0300

    Criando uma rota pai para melhor controle

--- src/routes.js
+++ src/routes.js
@@ -2,27 +2,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Inicio from "./pages/Inicio"
 import Favoritos from "./pages/Favoritos"
-import Cabecalho from './components/Cabecalho'
-import Rodape from './components/Rodape'
-import Container from "./components/Container"
-import FavoritosProvider from "./contextos/Favoritos"
 import Player from "./pages/Player"
 import NaoEncontrado from "./pages/NaoEncontrado"
+import PaginaBase from "./pages/PaginaBase/inde"
 function AppRouter() {
   return (
     <BrowserRouter>
-      <Cabecalho />
-      <Container>
-        <FavoritosProvider>
-          <Routes>
-            <Route path="/" element={<Inicio />}></Route>
-            <Route path="/favoritos" element={<Favoritos></Favoritos>}></Route>
-            <Route path="/:id" element={<Player></Player>}></Route>
-            <Route path="*" element={<NaoEncontrado></NaoEncontrado>}></Route>
-          </Routes>
-        </FavoritosProvider>
-      </Container>
-      <Rodape />
+      <Routes>
+        <Route path="/" element={<PaginaBase />}>
+          <Route index element={<Inicio />}></Route>
+          <Route path="favoritos" element={<Favoritos></Favoritos>}></Route>
+          <Route path=":id" element={<Player></Player>}></Route>
+          <Route path="*" element={<NaoEncontrado></NaoEncontrado>}></Route>
+        </Route>
+      </Routes>
     </BrowserRouter>
   )
 }
